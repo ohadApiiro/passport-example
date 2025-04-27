b9a11825e37ad69cbee3aff4306ed8fde56e12dc
ohad <ohad@apiiro.com>
2025-04-27 17:59:43 +0300

    a

--- routes.ts
+++ routes.ts
@@ -1,13 +1,15 @@
+import {handler} from "./handlers";
+
 const passport = require('passport');
-const handlers = require('handlers');
+//const handlers = require('handlers');
 
 
 module.exports = function (app, passport) {
-    app.get("/path1", passport.authorize, handlers.handler);
-    app.get("/path4", callAuth, handlers.handler);
-    app.get("/path-no-auth", handlers.handler);
+    app.get("/path1", passport.authorize, handler);
+    app.get("/path4", callAuth, handler);
+    app.get("/path-no-auth", handler);
 
-    app.get("/path2", myAuthorizationMiddleware, handlers.handler);
+    app.get("/path2", myAuthorizationMiddleware,handler);
 
     function myAuthorizationMiddleware(){
         passport.authorize();
@@ -18,7 +20,7 @@ module.exports = function (app, passport) {
     }
 
     const pauth = passport.authorize.bind()
-    app.get("/path3", pauth(), handlers.handler);
+    app.get("/path3", pauth(), handler);
 
-    app.get("/info", handlers.handler);
+    app.get("/info", handler);
 };
