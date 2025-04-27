4134b1bc363c72414bdb4c531ef78505a1a7aff5
ohad <ohad@apiiro.com>
2025-04-27 18:07:15 +0300

    a

--- handlers.ts
+++ handlers.ts
@@ -1,5 +1,15 @@
 
 
-export const handler = function (req, res) {
+export const handler1 = function (req, res) {
+};
+export const handler2 = function (req, res) {
+};
+export const handler3 = function (req, res) {
+};
+export const handler4 = function (req, res) {
+};
+
+
+export const handlerNoAuth = function (req, res) {
 
 };
--- routes.ts
+++ routes.ts
@@ -1,15 +1,15 @@
-import {handler} from "./handlers";
+import {handler1, handler2, handler3, handler4, handlerNoAuth} from "./handlers";
 
 const passport = require('passport');
 //const handlers = require('handlers');
 
 
 module.exports = function (app, passport) {
-    app.get("/path1", passport.authorize, handler);
-    app.get("/path4", callAuth, handler);
-    app.get("/path-no-auth", handler);
+    app.get("/path1", passport.authorize, handler1);
+    app.get("/path4", callAuth, handler4);
+    app.get("/path-no-auth", handlerNoAuth);
 
-    app.get("/path2", myAuthorizationMiddleware,handler);
+    app.get("/path2", myAuthorizationMiddleware, handler2);
 
     function myAuthorizationMiddleware(){
         passport.authorize();
@@ -20,7 +20,7 @@ module.exports = function (app, passport) {
     }
 
     const pauth = passport.authorize.bind()
-    app.get("/path3", pauth(), handler);
+    app.get("/path3", pauth(), handler3);
 
-    app.get("/info", handler);
+    // app.get("/info", handler1);
 };
